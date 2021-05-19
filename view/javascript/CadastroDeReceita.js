911e54f9417dc0b74c69d1a383926ecd5f418962
banzak1 <leosantanabr@gmail.com>
2021-05-19 12:25:55 -0300

    reestruturação das pastas

---
+++ view/javascript/CadastroDeReceita.js
@@ -0,0 +1,10 @@
+const test = document.getElementById('edit-change')
+const changeEdits = document.getElementsByTagName('input')
+
+
+test.addEventListener("click", function(){
+        changeEdits.classList.remove('disabled')
+        
+
+})
+
