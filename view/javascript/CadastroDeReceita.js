4e1c481f68eb7a645ed7b6ec618994bcfbccf303
banzak1 <leosantanabr@gmail.com>
2021-05-19 23:43:27 -0300

    criação da função para reorganizar o numero do campo

--- view/javascript/CadastroDeReceita.js
+++ view/javascript/CadastroDeReceita.js
@@ -20,14 +20,31 @@ saveEdit.addEventListener("click", function(){
 
 /*  Função para criar ou excluir novos modos de preparo */
 
-$('#prepare_mode_text').on('click', function() {
-        $('#line_1').clone().appendTo('.prepare_section');
+var counter = 2;
+        $("#test").on('click', function() {
+        $('<div id="test" class="prepare_mode"><span id="prepare_mode_number">'+counter+'</span> <input id="prepare_mode_text" type="text" class="inputText_mode" /><span id="delete_prepare_mode" class="material-icons">delete</span></div>').appendTo('.prepare_section');
+        counter++;
 });
 
 
+$(document).on('click', '#delete_prepare_mode', function() {
+        $(this).closest('.prepare_mode').remove();
+        counter--;
+        reset_values();
+})
 
-$(document).on('click', '#delete_prepare_mode', function(){
-        $(this).closest(".prepare_mode").remove();
-});
+function reset_values() {
+        var count = 1;
+        var del_icon = "delete";
+        //loop through all divs
+        $(".prepare_section > .prepare_mode ").each(function() {
+          $(this).find("span").text(count); //set new count to span
+          $(this).find("input").val(); //set value
+          $(this).find("span#delete_prepare_mode").text(del_icon); //set id
+          count++; //increment count
+        })
+      
+      
+}
 
 
